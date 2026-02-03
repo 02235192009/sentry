ea1e6566025f227d96306d812943a022607ce5e9
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2026-02-02 21:16:04 -0800

    pull CacheAccess out of the latest release handler for easier sharing

---
+++ src/sentry/workflow_engine/caches/cache_access.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+
+from sentry.utils.cache import cache
+
+
+class CacheAccess[T]:
+    """
+    Base class for type-safe naive cache access.
+    """
+
+    @abstractmethod
+    def key(self) -> str:
+        raise NotImplementedError
+
+    def get(self) -> T | None:
+        return cache.get(self.key())
+
+    def set(self, value: T, timeout: float | None) -> None:
+        cache.set(self.key(), value, timeout)
